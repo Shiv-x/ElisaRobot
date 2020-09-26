377f14e3d090e13035a02f5b5cf647c94b79bf55
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2020-09-26 10:03:34 +0530

    Update lyrics.py

--- haruka/modules/lyrics.py
+++ haruka/modules/lyrics.py
@@ -5,8 +5,8 @@ from tswift import Song
 from telegram import Bot, Update, Message, Chat
 from telegram.ext import run_async
 
-from tg_bot import dispatcher
-from tg_bot.modules.disable import DisableAbleCommandHandler
+from haruka import dispatcher
+from haruka.modules.disable import DisableAbleCommandHandler
 
 
 @run_async
