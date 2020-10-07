e15d36fc4aa9d17cef8471ea36fddc4f3119bb39
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2020-10-08 04:18:48 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -149,9 +149,9 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START
 
-    keyboard = [[InlineKeyboardButton(text="✔️ Add Elisa to your group", url="t.me/{}?startgroup=true".format(context.bot.username)]],
-    keyboard += [[InlineKeyboardButton(text="🛠 Reporting", callback_data="cntrl_panel_M"), InlineKeyboardButton(text="🇮🇳 Language", callback_data="set_lang_")], 
-                 [InlineKeyboardButton(text="❓ Help", callback_data="help_back"), InlineKeyboardButton(text="Source Code", url="https://github.com/QueenArzoo/ElisaRobot.git")]]
+    keyboard = [[InlineKeyboardButton(text="🇮🇳 Language", callback_data="set_lang_")]],
+    keyboard += [[InlineKeyboardButton(text="🛠 Reporting", callback_data="cntrl_panel_M")], 
+                 [InlineKeyboardButton(text="❓ Help", callback_data="help_back")]]
 
     update.effective_message.reply_text(PM_START.format(escape_markdown(first_name), bot.first_name), reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
 
