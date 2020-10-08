2f7e4b12139422a4568ca480268869701459f5e1
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2020-10-08 10:15:24 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -151,7 +151,8 @@ def send_start(bot, update):
 
     keyboard = [[InlineKeyboardButton(text="🇮🇳 Language", callback_data="set_lang_")]],
     keyboard += [[InlineKeyboardButton(text="🛠 Reporting", callback_data="cntrl_panel_M"), 
-                 InlineKeyboardButton(text="❓ Help", callback_data="help_back")]]
+                 InlineKeyboardButton(text="❓ Help", callback_data="help_back")],
+                 [InlineKeyboardButton(text="Source Code", url="https://github.com/QueenArzoo/JassicaRobot")]]
 
     update.effective_message.reply_text(PM_START.format(escape_markdown(first_name), bot.first_name), reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
 
